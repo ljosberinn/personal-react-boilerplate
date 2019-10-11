5d34718ffa6def36769ba2c7c9251ea980afd8ef
Gerrit Alex <galex@mpunkt.com>
2019-10-11 17:51:50 +0200

    removed dead transition container & assets

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
 } from 'react-router-dom';
 import { Title, Section } from 'rbx';
 import './assets/scss/app.scss';
-
 import { Loader } from './components';
 import PublicRoutesContainer, { PUBLIC_ROUTES } from './routes/public';
 //import PrivateRoutesContainer, { PRIVATE_ROUTES } from './routes/private';
@@ -16,53 +15,51 @@ import PublicRoutesContainer, { PUBLIC_ROUTES } from './routes/public';
 function App() {
   return (
     <Router>
-      <div className="transition-container">
-        <Switch>
-          <Route path={Object.keys(PUBLIC_ROUTES)} exact>
-            <PublicRoutesContainer>
-              <Suspense fallback={<Loader />}>
-                {Object.entries(PUBLIC_ROUTES).map(([path, component]) => (
-                  <Route path={path} component={component} key={path} />
-                ))}
-              </Suspense>
-            </PublicRoutesContainer>
-          </Route>
-          {/*<Suspense fallback={<Loader isFullPage />}>*/}
-          <Route path="/" exact>
-            <Section>
-              <Title>currently implemented routes</Title>
-              {[
-                {
-                  path: '/register',
-                  description: 'best practices Registration form',
-                },
-                {
-                  path: '/activate-account/token',
-                  description: 'Account activation route',
-                },
-                {
-                  path: '/login',
-                  description:
-                    'Regular login form, optionally accepting a mail as url argument to prefill the form',
-                },
-                {
-                  path: '/reset-password',
-                  description: 'Form to request a password reset link',
-                },
-              ].map(({ path, description }) => (
-                <Title as="h2" subtitle key={path}>
-                  <Link to={path}>{path}</Link> - {description}
-                </Title>
+      <Switch>
+        <Route path={Object.keys(PUBLIC_ROUTES)} exact>
+          <PublicRoutesContainer>
+            <Suspense fallback={<Loader />}>
+              {Object.entries(PUBLIC_ROUTES).map(([path, component]) => (
+                <Route path={path} component={component} key={path} />
               ))}
-            </Section>
-          </Route>
+            </Suspense>
+          </PublicRoutesContainer>
+        </Route>
+        {/*<Suspense fallback={<Loader isFullPage />}>*/}
+        <Route path="/" exact>
+          <Section>
+            <Title>currently implemented routes</Title>
+            {[
+              {
+                path: '/register',
+                description: 'best practices Registration form',
+              },
+              {
+                path: '/activate-account/token',
+                description: 'Account activation route',
+              },
+              {
+                path: '/login',
+                description:
+                  'Regular login form, optionally accepting a mail as url argument to prefill the form',
+              },
+              {
+                path: '/reset-password',
+                description: 'Form to request a password reset link',
+              },
+            ].map(({ path, description }) => (
+              <Title as="h2" subtitle key={path}>
+                <Link to={path}>{path}</Link> - {description}
+              </Title>
+            ))}
+          </Section>
+        </Route>
 
-          <Route>
-            <Redirect to="/" />
-          </Route>
-          {/*</Suspense>*/}
-        </Switch>
-      </div>
+        <Route>
+          <Redirect to="/" />
+        </Route>
+        {/*</Suspense>*/}
+      </Switch>
     </Router>
   );
 }
