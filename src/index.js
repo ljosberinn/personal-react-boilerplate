187de5326e37720706c893cde66ac143d66f1804
Gerrit Alex <galex@mpunkt.com>
2019-10-11 17:51:32 +0200

    moved bulma-checkradio import appropriately

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bulma-checkradio/dist/css/bulma-checkradio.min.css';
 
 render(<App />, document.getElementById('root'));
 
