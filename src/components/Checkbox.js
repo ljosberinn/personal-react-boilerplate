fd863123dbeb6e33980231ad5618d85f300a65a4
ljosberinn <admin@gerritalex.de>
2019-10-19 01:21:31 +0200

    improved Checkbox

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { sanitizeClassArray } from '../utils';
 
-function Checkbox({ className, ...rest }) {
+function Checkbox({ className, size, circled, blocked, borderless, ...rest }) {
   return (
     <input
       type="checkbox"
-      className={sanitizeClassArray(['is-checkradio', className])}
+      className={sanitizeClassArray([
+        'is-checkradio',
+        size && `is-${size}`,
+        circled && 'is-circle',
+        blocked && 'is-blocked',
+        borderless && 'has-no-border',
+        className,
+      ])}
       {...rest}
     />
   );
