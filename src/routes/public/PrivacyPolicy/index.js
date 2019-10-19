694e6b4c9c02d55195be2df50dd89695b8157b8d
ljosberinn <admin@gerritalex.de>
2019-10-19 14:13:33 +0200

    added react-helmet to route stubs

--- src/routes/public/PrivacyPolicy/index.js
+++ src/routes/public/PrivacyPolicy/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import { Section } from 'rbx';
 
 export default function PrivacyPolicy() {
-  return <h1>WIP</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>Privacy Policy | Brand Name</title>
+      </Helmet>
+      <Section>
+        <h1>WIP</h1>
+      </Section>
+    </>
+  );
 }
--- src/routes/public/TosRoute/index.js
+++ src/routes/public/TosRoute/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import { Section } from 'rbx';
 
 export default function TosRoute() {
-  return <h1>WIP</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>Terms of Service | Brand Name</title>
+      </Helmet>
+      <Section>
+        <h1>WIP</h1>
+      </Section>
+    </>
+  );
 }
