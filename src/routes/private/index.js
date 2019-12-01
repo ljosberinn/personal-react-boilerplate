66a1267ccb50669bcd5319414a6d81bab9d685cf
ljosberinn <admin@gerritalex.de>
2019-12-01 02:41:15 +0100

    added dummy private LandingPage

--- src/routes/private/index.js
+++ src/routes/private/index.js
@@ -2,6 +2,9 @@ import { lazy } from 'react';
 import * as ROUTES from '../../constants/routes';
 
 export const PRIVATE_ROUTES = {
+  [ROUTES.LANDING_PAGE.routerPath]: lazy(() =>
+    import(/* webpackChunkName: "private.landingpage" */ './LandingPage'),
+  ),
   [ROUTES.SETTINGS.routerPath]: lazy(() =>
     import(/* webpackChunkName: "private.settings" */ './SettingsRoute'),
   ),
