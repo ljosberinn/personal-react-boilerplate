e9301b496c9f34da85f708c6e8fb9b22785ac4dc
ljosberinn <admin@gerritalex.de>
2019-12-18 22:35:36 +0100

    version bumb, move config

--- .config/webpack.functions.js
+++
@@ -1,7 +0,0 @@
-const nodeExternals = require('webpack-node-externals');
-
-// https://github.com/netlify/netlify-lambda/issues/112
-module.exports = {
-  externals: [nodeExternals()],
-  mode: 'development',
-};
