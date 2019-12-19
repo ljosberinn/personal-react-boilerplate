e30041770d635a6ae7449d9f09ae356637757f6f
ljosberinn <admin@gerritalex.de>
2019-12-19 22:19:00 +0100

    revert navbar sizechange on widescreen

--- src/Layout.js
+++ src/Layout.js
@@ -12,11 +12,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Column.Group centered as="header">
-        <Column
-          widescreen={{ size: 9 }}
-          tablet={{ size: 12 }}
-          mobile={{ size: 12 }}
-        >
+        <Column>
           <Suspense fallback={null}>
             <Navbar />
           </Suspense>
