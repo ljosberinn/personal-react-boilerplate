f60a1d10dd6f03e54f8f070552b20b8f6b045c0f
ljosberinn <admin@gerritalex.de>
2019-12-28 18:02:42 +0100

    logo cleanup

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,15 +1,9 @@
-import { ReactComponent as ConfirmationSvg } from './confirmation.svg';
 import { ReactComponent as MailSvg } from './mail.svg';
-import { ReactComponent as ErrorSvg } from '../../assets/svg/error.svg';
-import { ReactComponent as LoadingSvg } from '../../assets/svg/loading.svg';
 import { ReactComponent as LocaleSvg } from '../../assets/svg/localization.svg';
 import { ReactComponent as LandingPageSvg } from '../../assets/svg/landingPage.svg';
 
 export {
-  ConfirmationSvg,
   MailSvg,
-  ErrorSvg,
-  LoadingSvg,
   LocaleSvg,
   LandingPageSvg,
 };
