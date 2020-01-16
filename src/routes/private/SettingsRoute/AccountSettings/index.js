914154b0b77806301b7a95ef31c195340c0e2c3d
Gerrit Alex <admin@gerritalex.de>
2020-01-16 23:01:27 +0100

    fix updatePassword again

--- src/routes/private/SettingsRoute/AccountSettings/index.js
+++ src/routes/private/SettingsRoute/AccountSettings/index.js
@@ -16,7 +16,7 @@ export default function AccountSettings() {
    * @param {string} password
    */
   async function updatePassword(password) {
-    await updateUser({ fields: { data: { password } } });
+    await updateUser({ data: { password } });
   }
 
   return (
