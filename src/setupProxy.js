ab4f8edd83d8076ae66318d3e456604437f10afc
Gerrit Alex <admin@gerritalex.de>
2020-02-19 02:09:27 +0100

    adapt to http-proxy-middleware 1.0

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    proxy('/.netlify/functions', {
+    createProxyMiddleware('/.netlify/functions', {
       target: 'http://localhost:9000',
       pathRewrite: {
         '^/\\.netlify/functions': '',
