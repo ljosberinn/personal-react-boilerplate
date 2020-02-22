3d5e43fca2b29cf5e38bb369a9facdd0b5c4bd5b
ljosberinn <admin@gerritalex.de>
2020-02-22 04:21:39 +0100

    fix isValidReferer

--- src/utils/lambdaUtils.js
+++ src/utils/lambdaUtils.js
@@ -1,4 +1,4 @@
 import { SITE_URL } from '../constants/env';
 
 export const isValidReferer = referer =>
-  referer.includes('localhost:3000') || referer === SITE_URL + '/';
+  referer.includes('localhost:3000') || referer.includes(SITE_URL);
