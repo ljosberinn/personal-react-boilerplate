f77f0124ebd834dc8ff54aee7bfb194921a92473
ljosberinn <admin@gerritalex.de>
2020-02-28 21:02:12 +0100

    fix test setup

--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,6 +1,6 @@
 import { render as rtlRender } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import React, { Suspense, StrictMode } from 'react';
+import React, { Suspense } from 'react';
 import { I18nextProvider, withTranslation } from 'react-i18next';
 import { IdentityContextProvider } from 'react-netlify-identity';
 import { Router } from 'react-router-dom';
@@ -25,17 +25,15 @@ export default function render(
 
   function Wrapper({ children }) {
     return (
-      <StrictMode>
-        <ThemeProvider>
-          <Router history={history}>
-            <IdentityContextProvider url={SITE_URL}>
-              <I18nextProvider i18n={i18n}>
-                <Suspense fallback={null}>{children}</Suspense>
-              </I18nextProvider>
-            </IdentityContextProvider>
-          </Router>
-        </ThemeProvider>
-      </StrictMode>
+      <ThemeProvider>
+        <Router history={history}>
+          <IdentityContextProvider url={SITE_URL}>
+            <I18nextProvider i18n={i18n}>
+              <Suspense fallback={null}>{children}</Suspense>
+            </I18nextProvider>
+          </IdentityContextProvider>
+        </Router>
+      </ThemeProvider>
     );
   }
 
