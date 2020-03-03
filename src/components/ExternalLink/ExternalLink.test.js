8b43388a78070ac3561444fa009ee4c1dda0fa5c
ljosberinn <admin@gerritalex.de>
2020-03-03 19:34:27 +0100

    add ExternalLink test

---
+++ src/components/ExternalLink/ExternalLink.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import render from '../../utils/testUtils';
+
+import ExternalLink from '.';
+
+const defaultProps = { href: '//gerritalex.de' };
+
+const Intercepti18n = ({ tReady, i18n, t, ...rest }) => (
+  <ExternalLink {...rest} />
+);
+
+describe('<ExternalLink />', () => {
+  it('should render without crasihng', () => {
+    render(<Intercepti18n {...defaultProps} />);
+  });
+
+  it('should have target="_blank" as default', () => {
+    const { queryByText } = render(
+      <Intercepti18n {...defaultProps}>test</Intercepti18n>,
+    );
+
+    const link = queryByText(/test/);
+
+    expect(link.getAttribute('target')).toBe('_blank');
+  });
+
+  it('should allow overriding target default', () => {
+    const { queryByText } = render(
+      <Intercepti18n {...defaultProps} target={undefined}>
+        test
+      </Intercepti18n>,
+    );
+
+    const link = queryByText(/test/);
+
+    expect(link.getAttribute('target')).toBe(null);
+  });
+
+  it('should have rel="noreferrer noopener" as default', () => {
+    const { queryByText } = render(
+      <Intercepti18n {...defaultProps}>test</Intercepti18n>,
+    );
+
+    const link = queryByText(/test/);
+
+    expect(link.getAttribute('rel')).toBe('noreferrer noopener');
+  });
+
+  it('should allow overriding rel defaults', () => {
+    const { queryByText } = render(
+      <Intercepti18n {...defaultProps} rel={undefined}>
+        test
+      </Intercepti18n>,
+    );
+
+    const link = queryByText(/test/);
+
+    expect(link.getAttribute('rel')).toBe(null);
+  });
+});
