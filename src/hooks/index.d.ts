eaea77af22374205c7ab43c8b031b2117fc61109
ljosberinn <admin@gerritalex.de>
2020-03-03 23:49:49 +0100

    add hooks types

---
+++ src/hooks/index.d.ts
@@ -0,0 +1,46 @@
+import { LocationDescriptorObject } from 'history';
+import { IconType } from 'react-icons';
+import { LoadableComponent } from 'react-loadable';
+
+interface Route {
+  routerPath: string;
+  clientPath: string;
+  component: LoadableComponent;
+  title: string;
+  visibleInDrawerNav: boolean;
+  icon?: IconType;
+}
+
+export function useNavigationContext(): {
+  PreloadingLink: React.Component<{ to: Route }>;
+  routes: {
+    [key: string]: Route;
+  };
+};
+
+export function useBreakpointVisibility(): [boolean, () => void];
+
+export function useLocationBasedVisibility(): [boolean, () => void];
+
+export function useDetectColorScheme(): 'dark' | 'light';
+
+export function useMediaQuery(query: string): boolean;
+
+export function useNavigate(): {
+  (path: string, state?: unknown): void;
+  (location: LocationDescriptorObject<unknown>): void;
+};
+
+export function usePrevious(initialValue: any): any;
+
+export function useScrollToTop(behaviour: ScrollBehavior): void;
+
+export function useTheme(): {
+  theme: 'light' | 'dark';
+  toggleTheme: () => void;
+};
+export function useServiceWorker(): {
+  isSupported: boolean;
+  registration?: ServiceWorkerRegistration;
+  options?: RegistrationOptions;
+};
