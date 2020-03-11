03d39c92ca1e77d1c0c2cfa2deccad6b691e2d93
ljosberinn <admin@gerritalex.de>
2020-03-11 23:38:00 +0100

    add Routes test

---
+++ src/routes/Routes.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import render from '../utils/testUtils';
+
+import Routes from '.';
+
+describe('<Routes />', () => {
+  it('should render without crashing', () => {
+    render(<Routes />);
+  });
+});
