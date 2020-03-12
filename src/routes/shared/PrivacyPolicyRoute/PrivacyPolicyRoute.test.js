6b1f5c7242d2c57255cdec33560b548b17f0f413
ljosberinn <admin@gerritalex.de>
2020-03-13 00:58:50 +0100

    add PrivacyPolicyRoute test

---
+++ src/routes/shared/PrivacyPolicyRoute/PrivacyPolicyRoute.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import render from '../../../utils/testUtils';
+
+import PrivacyPolicyRoute from '.';
+
+describe('<PrivacyPolicyRoute />', () => {
+  it('should render without crashing', () => {
+    render(<PrivacyPolicyRoute />);
+  });
+});
