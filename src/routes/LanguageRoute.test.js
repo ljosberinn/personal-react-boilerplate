661f3515ef70d5ba1a2cfeaf3824a756746c9b03
ljosberinn <admin@gerritalex.de>
2020-03-12 21:09:30 +0100

    add LanguageRoute test

---
+++ src/routes/LanguageRoute.test.js
@@ -0,0 +1,25 @@
+import { createMemoryHistory } from 'history';
+import i18n from 'i18next';
+import React from 'react';
+
+import { ENABLED_LANGUAGES } from '../constants/env';
+import render from '../utils/testUtils';
+import LanguageRoute from './LanguageRoute';
+
+describe('<LanguageRoute />', () => {
+  it('should render without crashing && do nothing on "/"', () => {
+    render(<LanguageRoute />);
+  });
+
+  ENABLED_LANGUAGES.forEach(language => {
+    const history = createMemoryHistory({
+      initialEntries: [`/${language}`],
+    });
+
+    it(`should render without crashing on a matching route (${language}) and change the language`, () => {
+      expect(i18n.language).not.toBe(language);
+      render(<LanguageRoute />, { history });
+      expect(i18n.language).toBe(language);
+    });
+  });
+});
