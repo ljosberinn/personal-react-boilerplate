6e3162291c51b0c01e73337b9c08eb79a296a59b
ljosberinn <admin@gerritalex.de>
2020-03-16 01:58:04 +0100

    add Unauthenticated Footer Links test

---
+++ src/components/Footer/UnauthenticatedLinks.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import render from '../../utils/testUtils';
+import UnauthenticatedLinks from './UnauthenticatedLinks';
+
+describe('<UnauthenticatedLinks />', () => {
+  it('renders without crashing', () => {
+    render(<UnauthenticatedLinks />);
+  });
+});
