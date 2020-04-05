487827d04235ef78125337b607c395c33c2b2477
ljosberinn <admin@gerritalex.de>
2020-04-06 01:36:17 +0200

    feat(components): add fullWidth prop to Spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,16 +1,46 @@
-import { Spinner as ChakraSpinner } from '@chakra-ui/core';
+import { Spinner as ChakraSpinner, Box } from '@chakra-ui/core';
 import React from 'react';
 import { useTimeout } from 'react-use';
 
 interface Props {
+  /**
+   * timeout in ms after which the spinner will show
+   */
   timeout?: number;
+  /**
+   * if true, covers the full page
+   */
+  fullWidth?: boolean;
 }
 
-export default function Spinner({ timeout = 0 }: Props) {
+export default function Spinner({ timeout = 0, fullWidth }: Props) {
   const [isReady] = useTimeout(timeout);
 
   if (!isReady()) {
     return null;
+  }
+
+  if (fullWidth) {
+    return (
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        width="100vw"
+        height="100vh"
+        position="absolute"
+        top={0}
+        left={0}
+        cursor="progress"
+      >
+        <ChakraSpinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="blue.500"
+          size="xl"
+        />
+      </Box>
+    );
   }
 
   return (
