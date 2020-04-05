5139fadc0dfced13b3aea7da82628a7d766a7464
ljosberinn <admin@gerritalex.de>
2020-04-06 01:35:24 +0200

    feat(component): add ExternalLink + test

---
+++ src/components/ExternalLink/ExternalLink.test.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { render } from '../../testUtils';
+import ExternalLink from './ExternalLink';
+
+const defaultProps = { href: '//gerritalex.de' };
+
+describe('<ExternalLink />', () => {
+  it('should render without crasihng', () => {
+    render(<ExternalLink {...defaultProps} />, { includeTranslation: false });
+  });
+
+  it('should have target="_blank" as default', () => {
+    const { queryByText } = render(
+      <ExternalLink {...defaultProps}>test</ExternalLink>,
+      { includeTranslation: false }
+    );
+
+    const link = queryByText(/test/);
+
+    expect(link.getAttribute('target')).toBe('_blank');
+  });
+
+  it('should allow overriding target default', () => {
+    const { queryByText } = render(
+      <ExternalLink {...defaultProps} target={undefined}>
+        test
+      </ExternalLink>,
+      { includeTranslation: false }
+    );
+
+    const link = queryByText(/test/);
+
+    expect(link.getAttribute('target')).toBe(null);
+  });
+
+  it('should have rel="noreferrer noopener" as default', () => {
+    const { queryByText } = render(
+      <ExternalLink {...defaultProps}>test</ExternalLink>,
+      { includeTranslation: false }
+    );
+
+    const link = queryByText(/test/);
+
+    expect(link.getAttribute('rel')).toBe('noreferrer noopener');
+  });
+
+  it('should allow overriding rel defaults', () => {
+    const { queryByText } = render(
+      <ExternalLink {...defaultProps} rel={undefined}>
+        test
+      </ExternalLink>,
+      { includeTranslation: false }
+    );
+
+    const link = queryByText(/test/);
+
+    expect(link.getAttribute('rel')).toBe(null);
+  });
+});
---
+++ src/components/ExternalLink/ExternalLink.tsx
@@ -0,0 +1,12 @@
+import { Link, Icon } from '@chakra-ui/core';
+import React, { PropsWithChildren } from 'react';
+
+type Props = PropsWithChildren<{ href: string; rel?: string; target?: string }>;
+
+export default function ExternalLink({ children, ...rest }: Props) {
+  return (
+    <Link isExternal rel="noreferrer noopener" color="teal.500" {...rest}>
+      {children} <Icon name="external-link" mx={1} />
+    </Link>
+  );
+}
