5c48ec286746b24f926b3e866ee65b84d4769cd8
ljosberinn <admin@gerritalex.de>
2020-04-08 02:33:19 +0200

    refactor(i18n-setup): disable i18next backend during early dev

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-chained-backend';
 import LocalStorageBackend from 'i18next-localstorage-backend';
-import BackendAdapter from 'i18next-multiload-backend-adapter';
-import XHR from 'i18next-xhr-backend';
+/*import BackendAdapter from 'i18next-multiload-backend-adapter';
+import XHR from 'i18next-xhr-backend';*/
 import { initReactI18next } from 'react-i18next';
 
 import { ENABLED_LANGUAGES } from './constants/env';
@@ -31,7 +31,7 @@ i18n
     backend: {
       backends: [
         LocalStorageBackend,
-        new BackendAdapter(null, {
+        /*new BackendAdapter(null, {
           backend: new XHR(null, {
             loadPath: '/.netlify/functions/geti18n?lng={{lng}}&ns={{ns}}',
             addPath:
@@ -41,19 +41,19 @@ i18n
               key: fallbackValue || '',
             }),
           }),
-        }),
+        }),*/
       ], // order defines lookup pattern
       backendOptions: [
         {
           prefix: 'i18n-',
           expirationTime: 28 * 24 * 60 * 60 * 1000,
         },
-        {},
+        //{},
       ],
     },
     ns: [], // removes 'translation' default key from backend query,
     whitelist: ENABLED_LANGUAGES,
-    saveMissing: true,
+    //saveMissing: true,
     react: {
       useSuspense: false,
     },
