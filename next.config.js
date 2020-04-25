e6db9f0a79c607ad2aae026e4f36a9040f5b440f
ljosberinn <admin@gerritalex.de>
2020-04-26 00:16:56 +0200

    feat: add preact

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const date = new Date();
 console.debug(`> Building on NODE_ENV="${process.env.NODE_ENV}"`);
 
 module.exports = nextSourceMaps({
+  experimental: {
+    modern: true,
+    polyfillsOptimization: true,
+  },
+
   env: {
     // core
     ENABLED_LANGUAGES: process.env.NEXT_PUBLIC_ENABLED_LANGUAGES,
@@ -19,12 +24,49 @@ module.exports = nextSourceMaps({
     BUILD_TIME: date.toString(),
     BUILD_TIMESTAMP: +date,
   },
-  webpack: (config, { isServer, buildId }) => {
+  webpack: (config, { dev, isServer, buildId }) => {
+    const splitChunks = config.optimization && config.optimization.splitChunks;
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups;
+      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test: preactModules,
+        });
+        cacheGroups.commons.name = 'framework';
+      } else {
+        cacheGroups.preact = {
+          name: 'commons',
+          chunks: 'all',
+          test: preactModules,
+        };
+      }
+    }
+
     config.plugins.push(
       new webpack.DefinePlugin({
         'process.env.NEXT_PUBLIC_SENTRY_RELEASE': JSON.stringify(buildId),
       })
     );
+
+    const aliases = config.resolve.alias || (config.resolve.alias = {});
+    aliases.react = aliases['react-dom'] = 'preact/compat';
+
+    if (!isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser';
+
+      // inject Preact DevTools
+      if (dev) {
+        const entry = config.entry;
+        config.entry = () =>
+          entry().then(entries => {
+            entries['main.js'] = ['preact/debug'].concat(
+              entries['main.js'] || []
+            );
+            return entries;
+          });
+      }
+    }
 
     return config;
   },
