2feef14586665c086799567fe70a80c039db4f22
ljosberinn <admin@gerritalex.de>
2020-05-16 03:45:30 +0200

    feat: remove default icons & brand colors from theme

--- src/client/theme.ts
+++ src/client/theme.ts
@@ -1,4 +1,19 @@
 import { theme } from '@chakra-ui/core';
 
 // see https://chakra-ui.com/theme for customization
-export default theme;
+// removes default icons as well as brand colors
+const { icons, colors: completeColors, ...rest } = theme;
+const {
+  twitter,
+  messenger,
+  linkedin,
+  whatsapp,
+  telegram,
+  facebook,
+  ...remainingColors
+} = completeColors;
+
+export default {
+  ...rest,
+  colors: remainingColors,
+};
