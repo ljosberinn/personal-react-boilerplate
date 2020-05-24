78107db599f99f4d29a6fbf1fedd2c6077cd30d0
ljosberinn <admin@gerritalex.de>
2020-05-24 21:10:09 +0200

    refactor: reduce Profile validator complexity

--- src/server/auth/provider/github/validator.ts
+++ src/server/auth/provider/github/validator.ts
@@ -1,5 +1,9 @@
-import { Profile } from 'passport-github2';
-export const isGithubProfile = (profile: object): profile is Profile =>
-  ['id', 'displayName', 'username', 'profileUrl', 'photos', 'emails'].every(
-    key => key in profile
-  );
+import { Profile as GithubProfile } from 'passport-github2';
+
+/**
+ * Determines whether the given object matches the GithubProfile interface
+ *
+ * @param {object} profile
+ */
+export const isGithubProfile = (profile: object): profile is GithubProfile =>
+  'profileUrl' in profile;
--- src/server/auth/provider/google/validator.ts
+++ src/server/auth/provider/google/validator.ts
@@ -1,37 +1,29 @@
-export interface GoogleProfile {
-  displayName: string;
+import { Profile } from 'passport';
+
+export interface GoogleProfile extends Profile {
   email: string;
   email_verified: boolean;
-  emails: { value: string; type: string }[];
   family_name: string;
   given_name: string;
-  id: string;
   language: string;
-  name: {
-    givenName: string;
-    familyName: string;
-  };
-  photos: { value: string; type: string }[];
   picture: string;
-  provider: 'google';
   sub: string;
   verified: boolean;
 }
 
+/**
+ * Determines whether the given object matches the GoogleProfile interface
+ *
+ * @param {object} profile
+ */
 export const isGoogleProfile = (profile: object): profile is GoogleProfile =>
   [
-    'displayName',
     'email',
     'email_verified',
-    'emails',
     'family_name',
     'given_name',
-    'id',
     'language',
-    'name',
-    'photos',
     'picture',
-    'provider',
     'sub',
     'verified',
   ].every(key => key in profile);
