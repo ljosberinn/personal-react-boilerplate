f7515cbe8ed3a52a856bfcb4d1273926ced48633
ljosberinn <admin@gerritalex.de>
2020-05-24 05:09:44 +0200

    fix: remove forgotten cleanup

--- src/pages/api/auth/callback/[provider].ts
+++ src/pages/api/auth/callback/[provider].ts
@@ -1,33 +1,17 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import nextConnect from 'next-connect';
-import passport from 'passport';
 
 import { Provider } from '../../../../client/context/AuthContext/AuthContext';
 import { ENABLED_PROVIDER } from '../../../../constants';
 import { encryptSession, setTokenCookie } from '../../../../server/auth/cookie';
-import { authMiddleware } from '../../../../server/auth/middleware';
+import {
+  authMiddleware,
+  promisifyAuthentication,
+} from '../../../../server/auth/middleware';
 import { isGithubProfile } from '../../../../server/auth/provider';
 import {
   NOT_FOUND,
   FOUND_MOVED_TEMPORARILY,
 } from '../../../../utils/statusCodes';
-
-const authenticate = (
-  method: Provider,
-  req: NextApiRequest,
-  res: NextApiResponse
-) =>
-  new Promise((resolve, reject) => {
-    const authenticator = passport.authenticate(method, {}, (error, token) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(token);
-      }
-    });
-
-    authenticator(req, res);
-  });
 
 export default nextConnect()
   .use(authMiddleware)
@@ -38,7 +22,7 @@ export default nextConnect()
       res.status(NOT_FOUND).end();
     }
 
-    const user = await authenticate(provider, req, res);
+    const user = await promisifyAuthentication(provider, req, res);
     const payload = getProfileData(user);
 
     const token = await encryptSession(payload);
