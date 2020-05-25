2a39e73760033a54c3918d82d3a2537c9c8c674b
ljosberinn <admin@gerritalex.de>
2020-05-25 02:16:15 +0200

    feat: api/auth
    - improve local api endpoint parsing
    - add signup endpoint

---
+++ src/pages/api/auth/signup.ts
@@ -0,0 +1,21 @@
+import nextConnect from 'next-connect';
+
+import { authMiddleware } from '../../../server/auth/middlewares';
+import { BAD_REQUEST, CREATED } from '../../../utils/statusCodes';
+
+export default nextConnect()
+  .use(authMiddleware)
+  .post(async (req, res) => {
+    if (req.body.length === 0) {
+      res.status(BAD_REQUEST).end();
+    }
+
+    const { username, password } = JSON.parse(req.body);
+
+    if (!username || !password) {
+      res.status(BAD_REQUEST).end();
+    }
+
+    // TODO: implement Profile interface here
+    res.status(CREATED).end({ json: { username } });
+  });
