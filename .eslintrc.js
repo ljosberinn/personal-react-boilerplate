bb258f5a6b2eb4e8621b711d25839eb4ac6c271e
ljosberinn <admin@gerritalex.de>
2020-05-27 00:05:58 +0200

    refactor: use personal eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,3 @@
 module.exports = {
-  extends: ['react-app', 'kentcdodds/jest'],
-  plugins: ['sort-keys-fix'],
-  rules: {
-    'import/order': [
-      'warn',
-      {
-        alphabetize: {
-          order: 'asc',
-        },
-        groups: [
-          ['builtin', 'external', 'internal'],
-          ['unknown', 'parent', 'sibling'],
-          'index',
-        ],
-        'newlines-between': 'always',
-      },
-    ],
-    'no-alert': 'error',
-    'no-console': 'warn',
-    'no-else-return': 'error',
-    'require-await': 'error',
-    'sort-keys-fix/sort-keys-fix': 'warn',
-  },
+  extends: ['galex'],
 };
