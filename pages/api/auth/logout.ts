8c1d7b82cd720388703e3b527435e1cf72c60309
ljosberinn <admin@gerritalex.de>
2020-05-28 19:03:58 +0200

    fix: dont set Location header on logout

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { removeTokenCookie } from '../../../src/server/auth/cookie';
-import { FOUND_MOVED_TEMPORARILY } from '../../../src/utils/statusCodes';
+import { OK } from '../../../src/utils/statusCodes';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   removeTokenCookie(res);
 
-  res.writeHead(FOUND_MOVED_TEMPORARILY, { Location: '/' }).end();
+  res.status(OK).end();
 }
