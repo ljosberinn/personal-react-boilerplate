5aadabfa94b77f101266dc63b373b5f04a638033
ljosberinn <admin@gerritalex.de>
2020-06-13 23:10:12 +0200

    fix: sentry setup
    - enable sentry env vars
    - add preliminary docs

--- docs/sentry/readme.md
+++ docs/sentry/readme.md
@@ -1,1 +1,6 @@
 https://github.com/vercel/next.js/blob/canary/examples/with-sentry/README.md
+
+NEXT_PUBLIC_SENTRY_DSN= project dsn
+SENTRY_ORG= organisation name from `Name` field in organizations
+SENTRY_PROJECT=project name
+SENTRY_AUTH_TOKEN= via https://sentry.io/settings/account/api/auth-tokens/
--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 const {
   NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
-  // SENTRY_ORG,
-  // SENTRY_PROJECT,
-  // SENTRY_AUTH_TOKEN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
   NODE_ENV,
 } = process.env;
 
@@ -27,9 +27,9 @@ module.exports = nextSourceMaps({
 
     if (
       SENTRY_DSN &&
-      // SENTRY_ORG &&
-      // SENTRY_PROJECT &&
-      // SENTRY_AUTH_TOKEN &&
+      SENTRY_ORG &&
+      SENTRY_PROJECT &&
+      SENTRY_AUTH_TOKEN &&
       NODE_ENV === 'production'
     ) {
       config.plugins.push(
