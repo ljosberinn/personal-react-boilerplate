f6d5045969bc0ccd4d93634786e2a657c9586377
ljosberinn <admin@gerritalex.de>
2020-06-16 21:23:54 +0200

    feat: improve PWA capabilities
    - add theme-agnostic meta.theme-color

--- src/client/components/common/ThemeSwitch/ThemeSwitch.tsx
+++ src/client/components/common/ThemeSwitch/ThemeSwitch.tsx
@@ -1,7 +1,10 @@
 import { Switch, useColorMode, Flex, FlexProps, Box } from '@chakra-ui/core';
+import Head from 'next/head';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { FaSun, FaMoon } from 'react-icons/fa';
+
+import theme from '../../../theme';
 
 const gray = 'gray.500';
 const yellow = 'yellow.500';
@@ -23,39 +26,45 @@ export default function ThemeSwitch(props: ThemeSwitchProps) {
   const { colorMode, toggleColorMode } = useColorMode();
   const { t } = useTranslation('theme');
 
+  const isLightTheme = colorMode === 'light';
+
   return (
-    <Flex cursor="pointer" d="inline-flex" {...props}>
-      <Box
-        d="inline-block"
-        as={FaSun}
-        height={6}
-        color={colorMap.sun[colorMode]}
-        data-testid="theme-switch-sun"
-        onClick={toggleColorMode}
-        aria-label={t(
-          colorMode === 'dark' ? 'set-light-theme' : 'is-light-theme'
-        )}
-        mr={2}
-      />
-      <Switch
-        aria-label="toggle theme"
-        isChecked={colorMode === 'dark'}
-        onChange={toggleColorMode}
-        display="flex"
-        alignItems="center"
-      />
-      <Box
-        d="inline-block"
-        as={FaMoon}
-        height={6}
-        color={colorMap.moon[colorMode]}
-        data-testid="theme-switch-moon"
-        onClick={toggleColorMode}
-        aria-label={t(
-          colorMode === 'light' ? 'set-dark-theme' : 'is-dark-theme'
-        )}
-        ml={2}
-      />
-    </Flex>
+    <>
+      <Head>
+        <meta
+          name="theme-color"
+          content={isLightTheme ? theme.colors.white : theme.colors.gray[800]}
+        />
+      </Head>
+      <Flex cursor="pointer" d="inline-flex" {...props}>
+        <Box
+          d="inline-block"
+          as={FaSun}
+          height={6}
+          color={colorMap.sun[colorMode]}
+          data-testid="theme-switch-sun"
+          onClick={toggleColorMode}
+          aria-label={t(isLightTheme ? 'is-light-theme' : 'set-light-theme')}
+          mr={2}
+        />
+        <Switch
+          aria-label="toggle theme"
+          isChecked={colorMode === 'dark'}
+          onChange={toggleColorMode}
+          display="flex"
+          alignItems="center"
+        />
+        <Box
+          d="inline-block"
+          as={FaMoon}
+          height={6}
+          color={colorMap.moon[colorMode]}
+          data-testid="theme-switch-moon"
+          onClick={toggleColorMode}
+          aria-label={t(isLightTheme ? 'set-dark-theme' : 'is-dark-theme')}
+          ml={2}
+        />
+      </Flex>
+    </>
   );
 }
--- src/client/components/common/ThemeSwitchAlt/ThemeSwitchAlt.tsx
+++ src/client/components/common/ThemeSwitchAlt/ThemeSwitchAlt.tsx
@@ -1,32 +1,43 @@
 import { useColorMode, Box, Button, ButtonProps } from '@chakra-ui/core';
+import Head from 'next/head';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { FaSun, FaMoon } from 'react-icons/fa';
+
+import theme from '../../../theme';
 
 export default function ThemeSwitchAlt(props: Omit<ButtonProps, 'children'>) {
   const { colorMode, toggleColorMode } = useColorMode();
   const { t } = useTranslation('theme');
 
-  const isLightMode = colorMode === 'light';
+  const isLightTheme = colorMode === 'light';
 
   return (
-    <Button
-      type="button"
-      onClick={toggleColorMode}
-      variant={isLightMode ? 'outline' : undefined}
-      role="checkbox"
-      aria-checked={!isLightMode}
-      aria-label={t(isLightMode ? 'set-dark-theme' : 'set-light-theme')}
-      border="none"
-      background="none"
-      _hover={{ backgroundColor: isLightMode ? 'gray.300' : 'gray.600' }}
-      {...props}
-    >
-      <Box
-        as={isLightMode ? FaSun : FaMoon}
-        aria-hidden="true"
-        color="yellow.500"
-      />
-    </Button>
+    <>
+      <Head>
+        <meta
+          name="theme-color"
+          content={isLightTheme ? theme.colors.white : theme.colors.gray[800]}
+        />
+      </Head>
+      <Button
+        type="button"
+        onClick={toggleColorMode}
+        variant={isLightTheme ? 'outline' : undefined}
+        role="checkbox"
+        aria-checked={!isLightTheme}
+        aria-label={t(isLightTheme ? 'set-dark-theme' : 'set-light-theme')}
+        border="none"
+        background="none"
+        _hover={{ backgroundColor: isLightTheme ? 'gray.300' : 'gray.600' }}
+        {...props}
+      >
+        <Box
+          as={isLightTheme ? FaSun : FaMoon}
+          aria-hidden="true"
+          color="yellow.500"
+        />
+      </Button>
+    </>
   );
 }
