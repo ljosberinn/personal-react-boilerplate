579df1bb140e007aa27b36aabb278a0903fd4a9b
ljosberinn <admin@gerritalex.de>
2020-06-17 20:32:05 +0200

    chore: deps
    - add eslint-plugin-tsc
    - change husky & lint-staged accordingly

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': 'prettier --write',
   '*.{js,tsx,ts,tsx,css,scss}': 'eslint --fix',
+  'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': 'prettier --write',
+  '*.{ts,tsx}':
+    'eslint --fix --plugin tsc --rule \'tsc/config: [2, {configFile: "./tsconfig.json"}]\'',
 };
