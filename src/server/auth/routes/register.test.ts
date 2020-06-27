b5d4dda9b6b26b0d003e0fa14d0c435d6671b50c
ljosberinn <admin@gerritalex.de>
2020-06-27 21:47:58 +0200

    test: register lambda

---
+++ src/server/auth/routes/register.test.ts
@@ -0,0 +1,85 @@
+import { testLambda, RequestMethods } from '../../../../testUtils/lambda';
+import { NOT_FOUND, BAD_REQUEST, CREATED } from '../../../utils/statusCodes';
+import { expectJSONBodyMiddleware } from '../../middlewares';
+import register from './register';
+
+const url = '/api/v1/auth/signup';
+const catchAllName = 'authRouter';
+const method: RequestInit['method'] = 'POST';
+const middleware = expectJSONBodyMiddleware;
+
+describe('api/signup', () => {
+  test('should be a function', () => {
+    expect(register).toBeInstanceOf(Function);
+  });
+
+  test('does nothing given no matching path', async () => {
+    const response = await testLambda(register, {
+      catchAllName,
+      middleware,
+      url,
+    });
+
+    expect(response.status).toBe(NOT_FOUND);
+  });
+
+  RequestMethods.filter(requestMethod => requestMethod !== method).forEach(
+    method => {
+      test(`does nothing on method "${method}"`, async () => {
+        const response = await testLambda(register, {
+          catchAllName,
+          method,
+          middleware,
+          url,
+        });
+
+        expect(response.status).toBe(NOT_FOUND);
+      });
+    }
+  );
+
+  ([{ username: 'foo' }, { password: 'foo' }] as Record<
+    string,
+    string
+  >[]).forEach(body => {
+    const affix = `(missing ${'password' in body ? 'username' : 'passsword'})`;
+
+    test(`responds with BAD_REQUEST given an incomplete dataset ${affix}`, async () => {
+      const response = await testLambda(register, {
+        body,
+        catchAllName,
+        method,
+        middleware,
+        url,
+      });
+
+      expect(response.status).toBe(BAD_REQUEST);
+    });
+  });
+
+  test('responds with CREATED given a complete dataset', async () => {
+    const response = await testLambda(register, {
+      body: { password: 'bar', username: 'foo' },
+      catchAllName,
+      method,
+      middleware,
+      url,
+    });
+
+    expect(response.status).toBe(CREATED);
+  });
+
+  test('responds with a JSON given a complete dataset', async () => {
+    const response = await testLambda(register, {
+      body: { password: 'bar', username: 'foo' },
+      catchAllName,
+      method,
+      middleware,
+      url,
+    });
+
+    const json = await response.json();
+
+    expect(json).toMatchObject(expect.any(Object));
+  });
+});
