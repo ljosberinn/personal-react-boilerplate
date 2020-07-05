3511346205dcf34c67e5d4ec9e28b004bf068d05
ljosberinn <admin@gerritalex.de>
2020-07-05 16:41:47 +0200

    test: add index smoketest

---
+++ src/client/routes/index.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Index from '../../../pages';
+import { render, act } from '../../../testUtils';
+
+describe('<Index />', () => {
+  /**
+   * when using chakra, components such as Popover or Menu will have effects -
+   * to catch them, we need  to do this stupid async dance here despite nothing
+   * actually being a promise
+   */
+  it('renders without crashing', async () => {
+    await act(async () => {
+      await render(<Index />);
+    });
+  });
+});
