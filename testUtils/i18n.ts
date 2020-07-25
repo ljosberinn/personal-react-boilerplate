b367c387f727e2a64ae16403bfdcd0142a65ef9b
Gerrit Alex <admin@gerritalex.de>
2020-07-25 18:15:53 +0200

    test: i18n
    - refactor i18n testing API
    - add MockI18n instance
    - improve docs
    - improve types

---
+++ testUtils/i18n.ts
@@ -0,0 +1,65 @@
+import { ResourceStore, i18n } from 'i18next';
+
+class MockStore implements ResourceStore {
+  off = jest.fn();
+  on = jest.fn();
+  options = {};
+  data = {};
+}
+
+export class MockI18n implements i18n {
+  init = jest.fn();
+  loadResources = jest.fn();
+  t = jest.fn(key => key);
+  use = jest.fn();
+  addResource = jest.fn();
+  addResourceBundle = jest.fn();
+  addResources = jest.fn();
+  changeLanguage = jest.fn();
+  cloneInstance = jest.fn();
+  createInstance = jest.fn();
+  dir = jest.fn();
+  emit = jest.fn();
+  getDataByLanguage = jest.fn();
+  getResource = jest.fn();
+  getFixedT = jest.fn();
+  getResourceBundle = jest.fn();
+  hasResourceBundle = jest.fn();
+  isInitialized = true;
+  loadLanguages = jest.fn();
+  loadNamespaces = jest.fn();
+  on = jest.fn();
+  reloadResources = jest.fn();
+  removeResourceBundle = jest.fn();
+  off = jest.fn();
+  setDefaultNamespace = jest.fn();
+  exists = jest.fn();
+  format = jest.fn();
+  language = 'en';
+  languages = [];
+  options = {};
+  reportNamespaces = {
+    addUsedNamespaces: jest.fn(),
+    getUsedNamespaces: jest.fn(),
+  };
+  modules = {
+    external: [],
+  };
+  services = {
+    backendConnector: undefined,
+    i18nFormat: undefined,
+    interpolator: {
+      init: jest.fn(),
+      interpolate: jest.fn(),
+      nest: jest.fn(),
+      reset: jest.fn(),
+      resetRegExp: jest.fn(),
+    },
+    languageDetector: undefined,
+    languageUtils: undefined,
+    logger: undefined,
+    pluralResolver: undefined,
+    resourceStore: new MockStore(),
+  };
+  store = new MockStore();
+}
--- testUtils/index.tsx
+++ testUtils/index.tsx
@@ -1,32 +1,95 @@
 import { ChakraProvider } from '@chakra-ui/core';
 import theme from '@chakra-ui/theme';
-import { render as rtlRender } from '@testing-library/react';
-import React, { cloneElement } from 'react';
-import { I18nextProvider, I18nContext } from 'react-i18next';
+import { render as rtlRender, RenderResult } from '@testing-library/react';
+import React, { cloneElement, ReactElement } from 'react';
+import { I18nextProvider, useTranslation } from 'react-i18next';
 
 import { AuthContextProvider } from '../src/client/context/AuthContext';
 import { AuthContextDefinition } from '../src/client/context/AuthContext/AuthContext';
 import { initI18Next } from '../src/client/i18n';
 import { i18nCache } from '../src/server/i18n';
 
-// may not be in seutpTests.js because lambda-Tests rely on node-fetch
-// which collides with whatwg-fetch
+// may not be in setupTests.js because lambda-Tests rely on node-fetch which
+// collides with whatwg-fetch
 import 'whatwg-fetch';
 
-const i18n = initI18Next({
+// as singleton, else you will see lots of `act` warnings in tests which are
+// technically unrelated to i18n
+const i18nInstance = initI18Next({
   i18nBundle: i18nCache['en'],
   i18nCache,
   language: 'en',
 });
 
-type TestOptions = {
+type Children = { children: ReactElement };
+
+type Namespace = Parameters<typeof useTranslation>[0];
+type I18NPropAlias = {
+  i18n?: string;
+  ready?: string;
+  t?: string;
+};
+
+export type TestOptions = {
   /**
-   * whether to include the i18n.t prop
-   * defaults to true
+   * allows using `t={jest.fn()} i18n={new MockI18n()} ready={true}` in tests for components
+   * that receive `t`, `i18n` and|or `ready` via props
+   *
+   * @example
+   * ```js
+   * // using `t` prop as is
+   * render(<MyComponent t={jest.fn()} />, {
+   *  i18n: {
+   *    namespace: 'my-namespace'
+   *  }
+   * });
+   *
+   * // using aliases in case i18n would overwrite existing props
+   * import { TFunction, i18n } from 'react-i18next';
+   *
+   * interface ComponentProps {
+   *   translate: TFunction;
+   *   i18nInstance: i18n;
+   * }
+   *
+   * render(<Component translate={jest.fn()} />, {
+   *  i18n: {
+   *    namespace: 'namespace',
+   *    alias: {
+   *      t: 'translate',
+   *      i18n: 'i18nInstance'
+   *    }
+   *  }
+   * });
+   * ```
+   *
    */
-  includeTranslation?: boolean;
+  i18n?: {
+    namespace: Namespace;
+    alias?: I18NPropAlias;
+  };
   /**
    * optional additional wrapper, e.g. Context
+   *
+   * @example
+   * ```ts
+   * // single wrapper
+   * render(<MyConponent />, {
+   *  wrapper: MyContext
+   * });
+   *
+   * // multiple wrapper
+   * render(<MyConponent />, {
+   *  wrapper: ({ children }) => (
+   *    <ContextA>
+   *      <ContextB>
+   *        {children}
+   *      <ContextB />
+   *    <ContextA />
+   *  )
+   * });
+   *
+   * ```
    */
   wrapper?: typeof ChildrenPassthrough;
   /**
@@ -35,39 +98,63 @@ type TestOptions = {
   session?: AuthContextDefinition['user'];
 };
 
-function ChildrenPassthrough({ children }: { children: JSX.Element }) {
+// UI-less passthrough fallback to prevent using conditional logic in render
+// below
+function ChildrenPassthrough({ children }: Children) {
   return children;
 }
 
+export interface I18nTestMiddlewareProps extends Children {
+  namespace: Namespace;
+  alias?: I18NPropAlias;
+}
+
+function I18nTestMiddleware({
+  children,
+  namespace,
+  alias = {},
+}: I18nTestMiddlewareProps) {
+  const { t, i18n, ready } = useTranslation(namespace);
+
+  const props = {
+    [alias.i18n || 'i18n']: i18n,
+    [alias.ready || 'ready']: ready,
+    [alias.t || 't']: t,
+  };
+
+  return cloneElement(children, props);
+}
+
+/**
+ * Custom render for @testing-library/react
+ *
+ * @see https://testing-library.com/docs/react-testing-library/setup#custom-render
+ * @param component the component under test
+ * @param options test options
+ */
 function render(
-  component: JSX.Element,
+  component: ReactElement,
   {
-    includeTranslation = true,
-    wrapper: AdditionalWrapper = ChildrenPassthrough,
+    i18n,
+    wrapper: Wrapper = ChildrenPassthrough,
     session = null,
   }: TestOptions = {}
-) {
-  return {
-    ...rtlRender(
-      <ChakraProvider theme={theme}>
-        <I18nextProvider i18n={i18n}>
-          <AuthContextProvider session={session}>
-            <AdditionalWrapper>
-              <I18nContext.Consumer>
-                {({ t }) =>
-                  // this allows usage of render(<Component some="prop" />) in tests
-                  // of components that receive `t` via props
-                  cloneElement(component, {
-                    t: includeTranslation ? t : undefined,
-                  })
-                }
-              </I18nContext.Consumer>
-            </AdditionalWrapper>
-          </AuthContextProvider>
-        </I18nextProvider>
-      </ChakraProvider>
-    ),
-  };
+): RenderResult {
+  return rtlRender(
+    <ChakraProvider theme={theme}>
+      <I18nextProvider i18n={i18nInstance}>
+        <AuthContextProvider session={session}>
+          <Wrapper>
+            {i18n ? (
+              <I18nTestMiddleware {...i18n}>{component}</I18nTestMiddleware>
+            ) : (
+              component
+            )}
+          </Wrapper>
+        </AuthContextProvider>
+      </I18nextProvider>
+    </ChakraProvider>
+  );
 }
 
 export * from '@testing-library/react';
