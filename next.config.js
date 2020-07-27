5327bb9fbf334202b669a9b04c931ddf6aaa1a61
Gerrit Alex <admin@gerritalex.de>
2020-07-27 22:29:45 +0200

    fix: disable experimental.modern

--- next.config.js
+++ next.config.js
@@ -85,9 +85,9 @@ const defaultConfig = {
     return config;
   },
   reactStrictMode: true,
-  experimental: {
-    modern: true,
-  },
+  // experimental: {
+  //   modern: true,
+  // },
 };
 
 module.exports = withPlugins([
