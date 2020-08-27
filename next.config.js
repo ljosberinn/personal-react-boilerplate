4cb0339ce75f5da4f966ac2f46456b5a970ca4c9
Gerrit Alex <admin@gerritalex.de>
2020-08-27 22:48:42 +0200

    refactor: sentry plugin
    - drop action, must have artifacts present
    - readd webpack plugin
    - attempt to use commit sha

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // Use the hidden-source-map option when you don't want the source maps to be
 // publicly available on the servers, only to the error reporting
 const withSourceMaps = require('@zeit/next-source-maps')();
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 const withOffline = require('next-offline');
 const { withPlugins } = require('next-compose-plugins');
 const { IgnorePlugin } = require('webpack');
@@ -8,9 +9,18 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+  VERCEL_GITHUB_COMMIT_SHA,
+} = process.env;
+
 const date = new Date();
 
-console.debug(`> Building on NODE_ENV="${process.env.NODE_ENV}"`);
+console.debug(`> Building on NODE_ENV="${NODE_ENV}"`);
 
 const offlineConfig = {
   target: 'serverless',
@@ -63,13 +73,43 @@ const defaultConfig = {
     BUILD_TIME: date.toString(),
     BUILD_TIMESTAMP: +date,
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, buildId }) => {
     if (!isServer) {
       config.resolve.alias['@sentry/node'] = '@sentry/react';
 
       serverOnlyPackages.forEach((package) => {
         config.plugins.push(new IgnorePlugin(new RegExp(package)));
       });
+    }
+
+    if (
+      NODE_ENV === 'production' &&
+      SENTRY_DSN &&
+      SENTRY_ORG &&
+      SENTRY_PROJECT &&
+      SENTRY_AUTH_TOKEN &&
+      VERCEL_GITHUB_COMMIT_SHA
+    ) {
+      config.plugins.push(
+        /**
+         * @see https://github.com/getsentry/sentry-webpack-plugin#options
+         */
+        new SentryWebpackPlugin({
+          include: '.next',
+          ignore: ['node_modules'],
+          urlPrefix: '~/_next',
+          release: buildId,
+          setCommits: {
+            repo: 'personal-react-boilerplate',
+            commit: VERCEL_GITHUB_COMMIT_SHA,
+          },
+          debug: true,
+          deploy: {
+            env: NODE_ENV,
+            started: +date,
+          },
+        })
+      );
     }
 
     return config;
