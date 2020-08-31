8c492f7444ddc8c6069baf0191602aa38cf88a1b
Gerrit Alex <admin@gerritalex.de>
2020-08-31 17:58:35 +0200

    fix(component): WebShareButton mobile icon

--- src/client/components/WebShareButton.tsx
+++ src/client/components/WebShareButton.tsx
@@ -33,7 +33,7 @@ export function WebShareButton({
   ...rest
 }: WebShareButtonProps): JSX.Element | null {
   if (
-    !IS_BROWSER ||
+    typeof navigator === 'undefined' ||
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     !navigator.share
   ) {
