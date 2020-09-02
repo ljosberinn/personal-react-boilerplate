ae2457a1adb84beaf552fe72d4906c1a4cf0ef49
Gerrit Alex <admin@gerritalex.de>
2020-09-03 01:37:47 +0200

    chore: deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
-module.exports = {
-  extends: 'galex',
-};
+const { createConfig } = require('eslint-config-galex/src/createConfig');
+
+/**
+ * read more on how to customize this config:
+ *
+ * @see https://github.com/ljosberinn/eslint-config-galex#i-went-through-30-eslint-plugins-so-you-dont-have-to
+ */
+module.exports = createConfig();
