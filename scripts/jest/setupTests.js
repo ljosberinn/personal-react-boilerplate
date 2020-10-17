d31f90d3fdf0f9bb0fe719460e601799cf28c703
Gerrit Alex <admin@gerritalex.de>
2020-10-17 05:53:27 +0200

    refactor(deps): use next internal env solution instead of dotenv directly

--- scripts/jest/setupTests.js
+++ scripts/jest/setupTests.js
@@ -1,16 +1,16 @@
+import { loadEnvConfig } from '@next/env';
 import { parse, serialize } from 'cookie';
-import { config } from 'dotenv';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-axe/extend-expect';
 
-config();
+loadEnvConfig(__dirname);
 
 /**
  * required to avoid having to declare dynamic env variables in
  * `./github/workflows/*.yml`
  * config files which would be needed there to allow tests to run on CI
  */
-(function assignMinimumReqEnvVariables() {
+(() => {
   process.env.NEXT_PUBLIC_ENABLED_LANGUAGES =
     process.env.NEXT_PUBLIC_ENABLED_LANGUAGES ?? 'en,de';
 
@@ -22,7 +22,7 @@ config();
     'local,github,google,facebook,discord';
 
   process.env.NEXT_PUBLIC_SESSION_LIFETIME =
-    process.env.NEXT_PUBLIC_SESSION_LIFETIME ?? 28800;
+    process.env.NEXT_PUBLIC_SESSION_LIFETIME ?? 28_800;
 
   process.env.NODE_ENV = process.env.NODE_ENV ?? 'test';
 })();
