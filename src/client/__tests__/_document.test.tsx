77f52aae61c68542355d0cd21c5f4c647b13842d
Gerrit Alex <admin@gerritalex.de>
2021-08-31 15:36:21 +0200

    Update _document.test.tsx

--- src/client/__tests__/_document.test.tsx
+++ src/client/__tests__/_document.test.tsx
@@ -1,59 +1,58 @@
-import NextDocument from 'next/document';
-import type { DocumentProps } from 'next/document';
-import { isValidElement } from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
+// import type { DocumentProps } from 'next/document';
+// import { isValidElement } from 'react';
+// import { renderToStaticMarkup } from 'react-dom/server';
 
-import { i18nCache } from '../../../testUtils/i18n';
-import { FALLBACK_LANGUAGE } from '../../constants';
-import Document from '../../pages/_document';
-import type { KarmaSSRProps } from '../karma/SSR';
+// import { i18nCache } from '../../../testUtils/i18n';
+// import { FALLBACK_LANGUAGE } from '../../constants';
+// import Document from '../../pages/_document';
+// import type { KarmaSSRProps } from '../karma/SSR';
 
-const pageProps: KarmaSSRProps = {
-  auth: { session: null },
-  cookies: '',
-  i18n: {
-    locale: FALLBACK_LANGUAGE,
-    resources: i18nCache,
-  },
-};
+// const pageProps: KarmaSSRProps = {
+//   auth: { session: null },
+//   cookies: '',
+//   i18n: {
+//     locale: FALLBACK_LANGUAGE,
+//     resources: i18nCache,
+//   },
+// };
 
-const defaultProps: DocumentProps = {
-  __NEXT_DATA__: {
-    appGip: true,
-    buildId: Math.floor(Math.random() * 1000).toString(),
-    page: '/',
-    props: { pageProps },
-    query: {},
-  },
-  ampPath: '/',
-  buildManifest: {
-    ampDevFiles: [],
-    ampFirstPages: [],
-    devFiles: [],
-    lowPriorityFiles: [],
-    pages: {
-      '/': [],
-      '/_app': [],
-    },
-    polyfillFiles: [],
-  },
-  canonicalBase: '/',
-  dangerousAsPath: '/',
-  devOnlyCacheBusterQueryString: '',
-  docComponentsRendered: {
-    Head: false,
-    Html: false,
-    Main: false,
-    NextScript: false,
-  },
-  dynamicImports: [],
-  headTags: [],
-  html: '',
-  hybridAmp: false,
-  inAmpMode: false,
-  isDevelopment: false,
-  scriptLoader: {},
-};
+// const defaultProps: DocumentProps = {
+//   __NEXT_DATA__: {
+//     appGip: true,
+//     buildId: Math.floor(Math.random() * 1000).toString(),
+//     page: '/',
+//     props: { pageProps },
+//     query: {},
+//   },
+//   ampPath: '/',
+//   buildManifest: {
+//     ampDevFiles: [],
+//     ampFirstPages: [],
+//     devFiles: [],
+//     lowPriorityFiles: [],
+//     pages: {
+//       '/': [],
+//       '/_app': [],
+//     },
+//     polyfillFiles: [],
+//   },
+//   canonicalBase: '/',
+//   dangerousAsPath: '/',
+//   devOnlyCacheBusterQueryString: '',
+//   docComponentsRendered: {
+//     Head: false,
+//     Html: false,
+//     Main: false,
+//     NextScript: false,
+//   },
+//   dynamicImports: [],
+//   headTags: [],
+//   html: '',
+//   hybridAmp: false,
+//   inAmpMode: false,
+//   isDevelopment: false,
+//   scriptLoader: {},
+// };
 
 describe.skip('<Document />', () => {
   /**
